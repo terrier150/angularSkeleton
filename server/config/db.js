4c39b6702fc003f42da14e2c08929a0312738326
oscarvazquez <oscar.vazquez2012@gmail.com>
2016-09-20 10:59:29 -0700

    database changes

--- server/config/db.js
+++ server/config/db.js
@@ -3,6 +3,26 @@ var mongoose = require('mongoose');
 var fs = require('fs');
 
 mongoose.connect('mongodb://localhost/newDatabaseName');
+/*
+*  CONNECTION EVENTS
+*  When successfully connected
+*/
+mongoose.connection.on( 'connected', function () {
+  console.log( `Mongoose default connection open to ${ dbURI }` );
+});
+/*
+*  If the connection throws an error
+*/
+mongoose.connection.on( 'error', function ( err ) {
+  console.error( `Mongoose default connection error: ${ err }` );
+});
+/*
+*  When the connection is disconnected
+*/
+mongoose.connection.on( 'disconnected', function () {
+  console.log( 'Mongoose default connection disconnected' );
+});
+
 
 var models_path = __dirname + "/../models"
 
